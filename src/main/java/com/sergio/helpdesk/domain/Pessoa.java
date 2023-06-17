aed70bf41ebb622c6e9984981c30bed255fc1a0b
Sergio Correa <stcf29@gmail.com>
2023-06-17 11:59:07 -0300

    anotacoes JPA para classes domain

--- src/main/java/com/sergio/helpdesk/domain/Pessoa.java
+++ src/main/java/com/sergio/helpdesk/domain/Pessoa.java
@@ -1,24 +1,53 @@
 package com.sergio.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sergio.helpdesk.domain.enums.Perfil;
 
-public abstract class Pessoa {
-	
+@Entity
+public abstract class Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
+	
+	
 	public Pessoa() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 	}
 	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
 		super();
@@ -27,6 +56,7 @@ public abstract class Pessoa {
 		this.cpf = cpf;
 		this.email = email;
 		this.senha = senha;
+		addPerfis(Perfil.CLIENTE);
 	}
 	public Integer getId() {
 		return id;
--- src/main/java/com/sergio/helpdesk/domain/Tecnico.java
+++ src/main/java/com/sergio/helpdesk/domain/Tecnico.java
@@ -3,15 +3,27 @@ package com.sergio.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.sergio.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@OneToMany(mappedBy= "tecnico")
 	private List <Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
