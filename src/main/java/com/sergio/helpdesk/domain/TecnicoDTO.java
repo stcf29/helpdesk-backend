2d3d2eb4875b0ddf8a485c0b84490405e1516ed7
Sergio Correa <sergio.filho@pdcase.com.br>
2024-02-24 12:35:59 -0300

    validação dos campos

--- src/main/java/com/sergio/helpdesk/domain/TecnicoDTO.java
+++ src/main/java/com/sergio/helpdesk/domain/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sergio.helpdesk.domain.enums.Perfil;
 
@@ -13,9 +15,16 @@ public class TecnicoDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	@NotNull(message = "O campo nome é requirido")
 	protected String nome;
+	
+	@NotNull(message = "O campo cpf é requirido")
 	protected String cpf;
+	
+	@NotNull(message = "O campo email é requirido")
 	protected String email;
+	
+	@NotNull(message = "O campo senha é requirido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	
