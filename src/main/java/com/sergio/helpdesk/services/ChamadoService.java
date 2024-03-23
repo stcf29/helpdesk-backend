db6bb57a6fc8cecfb744c88f053a0c9090fcf3af
Sergio Correa <sergio.filho@pdcase.com.br>
2024-03-23 10:15:56 -0300

    update chamado

--- src/main/java/com/sergio/helpdesk/services/ChamadoService.java
+++ src/main/java/com/sergio/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.sergio.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,6 +47,13 @@ public class ChamadoService {
 		return repository.save(newChamado(objDTO));
 	}
 	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+	
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
@@ -53,6 +61,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if(obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 		
 		chamado.setTecnico(tecnico);
@@ -64,4 +76,7 @@ public class ChamadoService {
 		return chamado;
 		
 	}
+
+
+	
 }
